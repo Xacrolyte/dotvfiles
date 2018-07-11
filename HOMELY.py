3d2fb0c2fc11afdcd5289196df4ddced42db7bc7
Vipul Gupta (@vipulgupta2048) <vipulgupta2048@gmail.com>
2018-07-11 11:39:45 +0530

    Main - tain - nence
    Signed-off-by: Vipul Gupta (@vipulgupta2048) <vipulgupta2048@gmail.com>

--- HOMELY.py
+++ HOMELY.py
@@ -2,4 +2,7 @@ from homely.files import symlink
 from homely.install import installpkg
 
 installpkg('nano',apt='nano')
+installpkg('screenfetch')
+symlink('.nanorc', '/etc/.nanorc')
+
 print("Finished Installing Nano Editor")
